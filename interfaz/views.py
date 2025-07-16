eb1ee10cc049c27e84c69f32f0e23e633a89125e
Shallow22 <fabriziojaradion@gmail.com>
2025-07-16 01:36:02 -0300

    Add dashboard and employee links

--- interfaz/views.py
+++ interfaz/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
+from django.db.models import Sum
+
 from inventario.models import Producto
+from compras.models import Compra
+from ventas.models import Venta
+from produccion.models import OrdenProduccion
+from finanzas.models import MovimientoFinanciero
+from clientes.models import Cliente
+from empleados.models import Empleado
 
 def home(request):
-    return render(request, 'interfaz/home.html')
+    context = {
+        'total_productos': Producto.objects.count(),
+        'total_compras': Compra.objects.count(),
+        'total_ventas': Venta.objects.count(),
+        'producciones_pendientes': OrdenProduccion.objects.filter(estado='pendiente').count(),
+        'balance_financiero': MovimientoFinanciero.objects.filter(tipo='ingreso').aggregate(total=Sum('monto'))['total'] or 0 -
+                             (MovimientoFinanciero.objects.filter(tipo='egreso').aggregate(total=Sum('monto'))['total'] or 0),
+        'total_clientes': Cliente.objects.count(),
+        'total_empleados': Empleado.objects.count(),
+    }
+    return render(request, 'interfaz/home.html', context)
 
 def inventario(request):
     productos = Producto.objects.all()
